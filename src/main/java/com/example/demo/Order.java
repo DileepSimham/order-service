9fa897140169643ba993cae61d1bdb4877db618f
DileepSimham <dileep.simham483@gmail.com>
2024-06-24 19:59:08 +0530

    aws code pipeline first commit

---
+++ src/main/java/com/example/demo/Order.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+	
+	private int id;
+	
+	private String name;
+	
+	private int quantity;
+	
+	private long price;
+
+}
